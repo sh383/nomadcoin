e78e001adc5a61ddaee380a8eef336f100e350d3
Sanghoon Shin <ssh.daniel@hanmail.net>
2023-01-25 22:22:45 +0900

    divide into templates

--- main.go
+++ main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 
 	"github.com/sh383/nomadcoin/blockchain"
 )
 
+const port string = ":4000"
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/pages/home.gohtml"))
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	tmpl.Execute(w, data)
+}
 func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	chain.AddBlock("Fourth Block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s \n", block.Data)
-		fmt.Printf("Hash: %s \n", block.Hash)
-		fmt.Printf("PrevHash: %s \n", block.PrevHash)
-	}
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on http:localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
