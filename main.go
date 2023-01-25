dabc6487ed38ce4bdbf7c4b1a9f32c5e914db52b
Sanghoon Shin <ssh.daniel@hanmail.net>
2023-01-25 22:51:57 +0900

    html finished

--- main.go
+++ main.go
@@ -1,28 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
+import "github.com/sh383/nomadcoin/explorer"
 
-	"github.com/sh383/nomadcoin/blockchain"
-)
-
-const port string = ":4000"
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/pages/home.gohtml"))
-	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	tmpl.Execute(w, data)
-}
 func main() {
-	http.HandleFunc("/", home)
-	fmt.Printf("Listening on http:localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	explorer.Start()
 }
