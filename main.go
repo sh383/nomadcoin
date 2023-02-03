f5de9cd0fef432c3b62ebe6a44b68826991f71e3
Sanghoon Shin <ssh.daniel@hanmail.net>
2023-02-03 19:11:50 +0900

    marshal text

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "github.com/sh383/nomadcoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port string = ":4000"
+
+type URLdescription struct {
+	URL         URL    `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload"`
+}
+
+type URL string
+
+func (u URL) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
+func documentation(w http.ResponseWriter, r *http.Request) {
+	data := []URLdescription{
+		{
+			URL:         URL("/"),
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL:         URL("/blocks"),
+			Method:      "POST",
+			Description: "Add A Block",
+			Payload:     "data:string",
+		},
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on https://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
